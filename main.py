d9a49cb2954259a44d87f5768bec1efcef77d07f
ruotingw <wruoting@gmail.com>
2019-08-28 23:44:08 -0400

    this is super cool ngl

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ from set import Set
 from point import Point
 from mpl_toolkits import mplot3d
 import numpy as np
+import matplotlib.pyplot as plt
 
 new_set = Set()
 
-new_set.add_point(Point(1,2,0))
-new_set.add_point(Point(1,3,0))
+new_set.add_point(Point(1,2,1))
+new_set.add_point(Point(1,3,1))
 new_set.add_point(Point(0,0,0))
 
+# https://jakevdp.github.io/PythonDataScienceHandbook/04.12-three-dimensional-plotting.html
 x = np.linspace(-6, 6, 30)
 y = np.linspace(-6, 6, 30)
 
 X, Y = np.meshgrid(x, y)
 Z = new_set.weight(X, Y)
 # 
-# fig = plt.figure()
-# ax = plt.axes(project='3d')
-# ax.contour3D()
-
-new_set.show
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X, Y, Z, 50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z');
+ax.view_init(60, 35)
+plt.show()
+# new_set.show
--- set.py
+++ set.py
@@ -23,17 +23,35 @@ class Set(object):
         :param y: y coordinate np array like
         :return: weight as a float
         """
-        np_point_array = self.vectorized_points(x, y)
-        point_weight_vectorize = np.vectorize(self.point_to_point_weight)
-        print(np_point_array)
-        print(self.two_dimensional_vectorized_graph)
-        [val, val2] = point_weight_vectorize(np_point_array, self.two_dimensional_vectorized_graph)
-        print(self.vectorized_graph)
-        print(self.two_dimensional_vectorized_graph)
-        print(np.ndim(self.two_dimensional_vectorized_graph))
-        # point_weight_vectorize(np_point_array, self.vectorized_graph)
-        # return np.divide(sigma_w_z, sigma_w)
+        # the input points to graph
+        np_point_array = self.vectorized_points(x, y).reshape(len(x)*len(y)) 
+        # nxn based on np point array to map to for our function
+        # it HAS to be a square matrix!!
+        # the points in our system
+        vectorized_graph_array = np.tile(self.vectorized_graph, (np.shape(np_point_array)[0] , 1))
+        # Transpose and then turn into [ p1 p1 p1 ... p1] [p2 p2 p2 ... p2] etc. n times
+        vectorized_graph_array_trans = vectorized_graph_array.transpose()
+        np.shape(vectorized_graph_array_trans)
         
+        ## Create our vectorize function
+        vectorized_point_weight_function = np.vectorize(self.point_weight_vectorize)
+        # for each point in our system, apply it to all points
+        # applied_matrix = np.empty((np.shape(vectorized_graph_array_trans)[0]*2,np.shape(vectorized_graph_array_trans)[1]))
+        sum_zi_wi = np.zeros(np.shape(vectorized_graph_array_trans)[1])
+        sum_wi = np.zeros(np.shape(vectorized_graph_array_trans)[1])
+        # this generates the weight times zi and the weight for each input point to point ratio
+        for index, data_point_row in enumerate(vectorized_graph_array_trans):
+            weight_wi_zi, weight_xi = vectorized_point_weight_function(data_point_row, np_point_array)
+            # all of the even indices are wi/zi and all the odd indices are wi
+            # add all even indices together and all the odd indices together as well
+            sum_zi_wi = np.add(sum_zi_wi, weight_wi_zi)
+            sum_wi = np.add(sum_wi, weight_xi)
+        
+        madge_vector = np.divide(sum_zi_wi, sum_wi)            
+        # need to reshape this madge vector
+        return madge_vector.reshape(len(x),len(y))
+                
+    
     def add_point(self, point):
         """
         Adds another point to the set. The set will be an undirected graph with nC2 total edges
@@ -47,15 +65,15 @@ class Set(object):
             self.graph[point] = {}
             # Add point to np array
             self.vectorized_graph = np.append(self.vectorized_graph, point)
-            # Make into a 2x2 d array
-            self.two_dimensional_vectorized_graph = np.tile(self.vectorized_graph, (len(self.vectorized_graph), 1))
+
             
-    def point_to_point_weight(self, point, graph_point):
+    def point_weight_vectorize(self, graph_point, point):
         # We could probably optimize here with some array x array apply methods with numpy
         distance = euclidean(point.tuple, graph_point.tuple)
         w_i = gaussian_area(distance, self.mean, self.sigma)
         z_i = graph_point.type
-        return [np.multiply(w_i, z_i), w_i]
+        #(np.multiply(w_i, z_i), w_i)
+        return np.multiply(w_i, z_i), w_i
             
     @staticmethod
     def vectorized_points(x, y):
