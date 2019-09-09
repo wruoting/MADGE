bc9cdbd72b2aa8ea52784d610f629c947b674676
ruotingw <wruoting@gmail.com>
2019-09-09 19:46:07 -0400

    graphing ready

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from point import Point
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
+import ast
 
 def add_points_and_graph(data_set, linspace):
     """
@@ -18,13 +19,19 @@ def add_points_and_graph(data_set, linspace):
     x_0 = []
     y_0 = []
     z_0 = []
+    classification_a = 1
+    classification_b = 0
     for data_point in data_set:
         new_set.add_point(Point(data_point[0], data_point[1], data_point[2]))
-        if data_point[2] == 1:
+        if data_point[2] != 1:
+            classification_a = data_point[2]
+        if data_point[2] != classification_a:
+            classification_b = data_point[2]
+        if data_point[2] == classification_a:
             x_1.append(data_point[0])
             y_1.append(data_point[1])
             z_1.append(0)
-        elif data_point[2] == 0:
+        else:
             x_0.append(data_point[0])
             y_0.append(data_point[0])
             z_0.append(0)
@@ -39,9 +46,9 @@ def add_points_and_graph(data_set, linspace):
 
     ax = plt.axes(projection='3d')
     # The red is the 1 classification
-    ax.scatter(x_1, y_1, z_1, c="#ff0000", label="1")
+    ax.scatter(x_1, y_1, z_1, c="#ff0000", label=classification_a)
     # The blue is the 0 classification
-    ax.scatter(x_0, y_0, z_0, c="#003399", label="0")
+    ax.scatter(x_0, y_0, z_0, c="#003399", label=classification_b)
     # For contouring
     # ax.contour3D(X, Y, Z, 50, cmap='binary')
     # For surface plots
@@ -55,14 +62,19 @@ def add_points_and_graph(data_set, linspace):
 
 def add_points_to_data_set():
     f= open("./SampleData/circle_classification.txt","r")
-    return np.array(f.readlines()[0])
+    return np.array(ast.literal_eval(f.readlines()[0]))
+
+def convert_array_to_array_of_tuples(input_array):
+    to_tuple = lambda v: tuple(v)
+    return [to_tuple(ai) for ai in input_array]
 
 # data_set = [(5,5,1), (1,1,1), (0,0,0), (-5,-5,1), (-3,-3,1)]
 
 linspace = (-6, 6, 30)
-training_data_set = add_points_to_data_set()
+training_data_set = convert_array_to_array_of_tuples(add_points_to_data_set())
 add_points_and_graph(training_data_set, linspace)
 
 # add_points_and_graph(data_set, linspace)
+
 
 
